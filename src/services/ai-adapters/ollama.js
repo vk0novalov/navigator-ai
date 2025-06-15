f1069c1940f88bc84434274a3ff90bf6de43f0f8
Victor Konovalov <victor@konovalov.pw>
2025-06-15 11:21:10 +0300

    feat: add support of remote ollama

--- src/services/ai-adapters/ollama.js
+++ src/services/ai-adapters/ollama.js
@@ -1,24 +1,34 @@
-import ollama from 'ollama';
+import ollama, { Ollama } from 'ollama';
 
 const EMBED_MODEL = 'bge-m3';
 const CLASSIFIER_MODEL = 'llama3.2:3b';
 
+const { OLLAMA_URL } = process.env;
+
+const ollamaClient = OLLAMA_URL ? new Ollama({ host: OLLAMA_URL }) : ollama;
+export default ollamaClient;
+
+// Useful for simultaneous requests
+export function createOllamaClient() {
+  return OLLAMA_URL ? new Ollama({ host: OLLAMA_URL }) : new Ollama();
+}
+
 // Ensure the models are pulled before using it
 const requiredModels = [EMBED_MODEL, CLASSIFIER_MODEL];
-const { models } = await ollama.list().catch(() => {
+const { models } = await ollamaClient.list().catch(() => {
   console.error('❌ Ollama is not running or not accessible.');
   process.exit(1);
 });
 for (const model of requiredModels) {
   if (!models.some((m) => m.name.startsWith(model))) {
     console.log(`🔄 Pulling model: ${model}`);
-    await ollama.pull({ model });
+    await ollamaClient.pull({ model });
     console.log(`✅ Model ${model} is ready for use.`);
   }
 }
 
 export async function embed(text, { truncate = false } = {}) {
-  const result = await ollama.embed({
+  const result = await ollamaClient.embed({
     model: EMBED_MODEL,
     input: text,
     truncate, // we use smart chunking, so no need to truncate by default
@@ -27,7 +37,7 @@ export async function embed(text, { truncate = false } = {}) {
 }
 
 export async function question(text) {
-  const result = await ollama.chat({
+  const result = await ollamaClient.chat({
     model: CLASSIFIER_MODEL,
     messages: [{ role: 'user', content: text }],
   });
